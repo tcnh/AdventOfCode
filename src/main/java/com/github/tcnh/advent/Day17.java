166d1e1978edd97f9a7a7ee87f7bdf0c3245af01
Tom Heintzberger <tom.heintzberger@praegus.nl>
2017-12-17 10:15:11 +0100

    Day 17 - fix

--- src/main/java/com/github/tcnh/advent/Day17.java
+++ src/main/java/com/github/tcnh/advent/Day17.java
@@ -5,17 +5,19 @@ import java.util.List;
 
 class Day17 {
 
+
     static int firstAnswer() {
         List<Integer> buffer = new ArrayList<>();
         buffer.add(0);
         int steps = 344;
         int position = 0;
-        for(int i = 0; i <= 2017; i++) {
-            position = (position + 1 + steps) % buffer.size();
+        for(int i = 1; i <= 2017; i++) {
+            position = (position + steps) % buffer.size() + 1;
             buffer.add(position, i);
         }
         return buffer.get(position + 1);
     }
+
 
     static int secondAnswer() {
         int steps = 344;
